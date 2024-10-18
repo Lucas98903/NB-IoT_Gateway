9ba5b4d0188727834f7535d4b6f63618b56f4e7a
Lucas Henrique <161077864+Lucas98903@users.noreply.github.com>
2024-10-17 21:03:16 -0300

    Updated- MEMORY

--- services/memory/memory.py
+++ services/memory/memory.py
@@ -1,5 +1,6 @@
 import json
 from pydantic import BaseModel
+from datetime import datetime
 
 class Memory:
     def __init__(self):
@@ -7,9 +8,25 @@ class Memory:
         self.address = None
 
     def storage_data(self, data, address):
+        # Verifica se o dado é uma instância de BaseModel (Pydantic) e converte para dicionário
         if isinstance(data, BaseModel):
             data = data.dict()
-            
+
+        # Função recursiva para converter datetime para string no formato ISO 8601
+        def convert_datetime(item):
+            if isinstance(item, dict):
+                return {k: convert_datetime(v) for k, v in item.items()}
+            elif isinstance(item, list):
+                return [convert_datetime(v) for v in item]
+            elif isinstance(item, datetime):
+                return item.isoformat()
+            else:
+                return item  # Retorna o valor sem modificação se não for datetime
+
+        # Converte apenas se houver datetime
+        data = convert_datetime(data)
+
+        # Salva os dados no arquivo
         with open(address, "w") as file:
             json.dump(data, file)
 
@@ -20,4 +37,3 @@ class Memory:
             return read
         except FileNotFoundError:
             return None
-        
