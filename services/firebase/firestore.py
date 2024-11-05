8d5fa2412e4e24225fa5f1848a73f88acea3a968
Lucas98903 <161077864+Lucas98903@users.noreply.github.com>
2024-11-05 18:51:19 -0300

    ADD - log in firebase

--- services/firebase/firestore.py
+++ services/firebase/firestore.py
@@ -1,24 +1,33 @@
 import firebase_admin
+import traceback
+
+from services.logger import log
 from firebase_admin import credentials, firestore
 
-address = r"D:\OneDrive\Projeto - Sensor indutivo\NB-IoT_Gateway\NB-IoT_Gateway\services\firebase\service_account_key.json"
 
-cred = credentials.Certificate(address)
-firebase_admin.initialize_app(cred)
+try:
+    address = r"D:\OneDrive\Projeto - Sensor indutivo\NB-IoT_Gateway\NB-IoT_Gateway\services\firebase\service_account_key.json"
 
-db = firestore.client()
+    cred = credentials.Certificate(address)
+    firebase_admin.initialize_app(cred)
 
+    db = firestore.client()
 
-def update_park(alarm_park):
-    # Reference -> Collection | Document ID
-    doc_ref = db.collection("status_park").document("Inatel")
+    def update_park(alarm_park):
+        # Reference -> Collection | Document ID
+        doc_ref = db.collection("status_park").document("Inatel")
 
-    doc_ref.update({
-        "001": alarm_park
-    })
+        doc_ref.update({
+            "001": alarm_park
+        })
 
-
-# To test the code
-if __name__ == "__main__":
-    alarm_park = False
-    update_park(alarm_park)
+    # To test the code
+    if __name__ == "__main__":
+        alarm_park = False
+        update_park(alarm_park)
+except Exception as e:
+    detail_error = traceback.format_exc()
+    log.logger.error(
+        f"Error in Firebase: {e} \n DETAIL ERROR \n {
+            detail_error}"
+    )
